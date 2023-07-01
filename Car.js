ab31139b173c98023f2abe91c79d7bc446094092
LocTranTran <136582921+LocTranTran@users.noreply.github.com>
2023-07-01 21:32:56 +0700

    Update Car.js

--- Car.js
+++ Car.js
@@ -1,13 +1,1 @@
-slideCard = document.querySelector('.slide__card');
-let isDown = false;
-let startX;
-let startY;
-let scrollLeft;
 
-slideCard.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk;
-});
